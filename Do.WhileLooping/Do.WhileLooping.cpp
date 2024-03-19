ca6698d887f292b57f4d8fc6d99e9f4fe6923f55
syairaarif <putrisyafira4444@gmail.com>
2024-03-19 11:12:32 +0700

    mambuat variabel untuk perulangan do while

---
+++ Do.WhileLooping/Do.WhileLooping.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int x;
+    srand(time(0));
+
+    x = rand() % 10;
+
+    cout << "nilai x awal : " << x << endl;
+
+    
+}
+
