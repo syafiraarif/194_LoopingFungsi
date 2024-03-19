c998f4bad9327746254b8e62338094832f918c21
syairaarif <putrisyafira4444@gmail.com>
2024-03-19 10:57:39 +0700

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    int x;
+    srand(time(0));
+
+    x = rand() % 10;
+
+    cout << "nilai x awal : " << x << endl;
+
+    while (x > 4)
+    {
+
+        cout << "perulangan while" << endl;
+        x = rand() % 10;
+
+        cout << "nilai x : " << x << endl;
+    }
+}
---
+++ ProjectPerulangan/ProjectPerulangan.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    int arr[5];
+
+    for (i = 60; i > 10; i -=10)
+    {
+        cout << i << "selamat pagi dunia" << endl;
+    }
+
+    cout << "nilai i terakhir : " << i << endl;
+
+    
+}
